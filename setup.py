ebe31f83d48a0c599878f3bf2dd62a1dcb86acbe
Felix Yan <felixonmars@archlinux.org>
2016-01-07 10:27:38 +0800

    Clean up setup.py and bump to 0.1.3

--- setup.py
+++ setup.py
@@ -1,32 +1,34 @@
-from setuptools import setup, Extension
-import distutils.core
-import sys
-import os
+from setuptools import setup
 
 setup(
-        name = 'tldr',
-        version = "0.1.2",
-        author = 'Felix Yan',
-        author_email = 'felixonmars@gmail.com',
-        url = 'https://github.com/felixonmars/tldr-python-client',
-        description = 'command line client for tldr',
-        license = 'MIT',
-        py_modules = ['tldr'],
-        scripts=['tldr.py'],
-        install_requires = ['six', 'termcolor', 'colorama'],
-        entry_points = { 'console_scripts': [ 'tldr = tldr:main' ] },
-        classifiers = [
-                "Development Status :: 4 - Beta",
-                "License :: OSI Approved :: MIT License",
-                "Environment :: Console",
-                "Intended Audience :: End Users/Desktop",
-                "Natural Language :: English",
-                "Operating System :: POSIX :: Linux",
-                "Operating System :: MacOS :: MacOS X",
-                "Operating System :: POSIX :: SunOS/Solaris",
-                "Programming Language :: Python :: 2.7",
-                "Programming Language :: Python :: 3.3",
-                "Topic :: Utilities",
-                "Topic :: System"
-        ]
+    name='tldr',
+    version="0.1.3",
+    author='Felix Yan',
+    author_email='felixonmars@gmail.com',
+    url='https://github.com/felixonmars/tldr-python-client',
+    description='command line client for tldr',
+    license='MIT',
+    py_modules=['tldr'],
+    scripts=['tldr.py'],
+    install_requires=['six', 'termcolor', 'colorama'],
+    tests_require=[
+        'pytest-runner',
+    ],
+    entry_points={
+        'console_scripts': ['tldr = tldr:main']
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: SunOS/Solaris",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Utilities",
+        "Topic :: System"
+    ]
 )
