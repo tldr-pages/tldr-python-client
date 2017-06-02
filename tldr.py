7886bc3797fe74704fe9f5d790be75f0401662e3
Ilia Konnov <iliakonnov@gmail.com>
2017-06-02 11:56:29 +0300

    Changed path to cache dir. Closes #37

--- tldr.py
+++ tldr.py
@@ -71,10 +71,17 @@ os_directories = {
 }
 
 
+def get_cache_dir():
+    if not os.environ.get('XDG_CACHE_HOME', False):
+        if not os.environ.get('HOME', False):
+            return os.path.join(os.path.expanduser("~"), ".cache", "tldr")
+        return os.path.join(os.environ.get('HOME'), '.cache', 'tldr')
+    return os.path.join(os.environ.get('XDG_CACHE_HOME'), 'tldr')
+
+
 def get_cache_file_path(command, platform):
     cache_file_name = command + "_" + platform + ".md"
-    cache_file_path = os.path.join(
-        os.path.expanduser("~"), ".tldr_cache", cache_file_name)
+    cache_file_path = os.path.join(get_cache_dir(), cache_file_name)
     return cache_file_path
 
 
@@ -234,7 +241,7 @@ def output(page):
 
 
 def update_cache():
-    cache_path = os.path.join(os.path.expanduser("~"), ".tldr_cache")
+    cache_path = get_cache_dir()
     if not os.path.exists(cache_path):
         return
     files = [file_name for file_name in os.listdir(cache_path)
