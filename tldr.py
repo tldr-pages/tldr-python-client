caabb862fd6eb2106eba17b7aaba1f99be232453
Matthew Peveler <matt.peveler@gmail.com>
2018-06-03 12:08:31 -0400

    Update tldr.py

--- tldr.py
+++ tldr.py
@@ -274,8 +274,8 @@ def main():
                         nargs=1,
                         default=None,
                         type=str,
-                        choices=['linux', 'osx', 'sunos', 'windows'],
-                        help="Override the operating system [linux, osx, sunos, windows]")
+                        choices=['linux', 'osx', 'sunos'],
+                        help="Override the operating system [linux, osx, sunos]")
 
     parser.add_argument('-s', '--source',
                         default=DEFAULT_REMOTE,
