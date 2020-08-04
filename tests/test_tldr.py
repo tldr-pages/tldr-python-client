eab5edbb28764d4cb96474c018a0145f52516b60
samuel-w <samuel.woon@protonmail.com>
2020-08-04 01:05:45 -0500

    Add error message test

--- tests/test_tldr.py
+++ tests/test_tldr.py
@@ -3,6 +3,8 @@ import io
 import types
 import unittest
 import tldr
+import pytest
+from mock import patch
 
 
 class TLDRTests(unittest.TestCase):
@@ -12,7 +14,7 @@ class TLDRTests(unittest.TestCase):
                 old_stdout = sys.stdout
                 sys.stdout = io.StringIO()
                 sys.stdout.buffer = types.SimpleNamespace()
-                sys.stdout.buffer.write = lambda x: sys.stdout.write(x.decode('utf-8'))
+                sys.stdout.buffer.write = lambda x: sys.stdout.write(x.decode("utf-8"))
                 tldr.output(f_original)
 
                 sys.stdout.seek(0)
@@ -21,3 +23,13 @@ class TLDRTests(unittest.TestCase):
 
                 correct_output = f_rendered.read()
                 self.assertEqual(tldr_output, correct_output)
+
+    def test_error_message(self):
+        testargs = ["tldr", "73eb6f19cd6f"]
+        with patch.object(sys, "argv", testargs):
+            with pytest.raises(SystemExit) as pytest_wrapped_e:
+                tldr.main()
+            correct_output = "`73eb6f19cd6f` documentation is not available. Consider contributing Pull Request to https://github.com/tldr-pages/tldr"  # noqa
+            print("Test {}".format(pytest_wrapped_e))
+            assert pytest_wrapped_e.type == SystemExit
+            assert str(pytest_wrapped_e.value) == correct_output
