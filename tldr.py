f31595ef91e6f7c8b0aacfb6d6f09e7efb8ca01e
Xiang Wang <ramwin@qq.com>
2021-01-04 17:47:01 +0800

    get remote resource only after all cache failed

--- tldr.py
+++ tldr.py
@@ -46,6 +46,11 @@ OS_DIRECTORIES = {
 }
 
 
+class CacheNotExist(Exception):
+
+    pass
+
+
 def get_language_code(language):
     language = language.split('.')[0]
     if language in ['pt_PT', 'pt_BR', 'zh_TW']:
@@ -129,11 +134,15 @@ def get_page_url(command, platform, remote, language):
     return remote + language + "/" + platform + "/" + quote(command) + ".md"
 
 
-def get_page_for_platform(command, platform, remote, language):
+def get_page_for_platform(command, platform, remote, language, only_use_cache=False):
     data_downloaded = False
     if USE_CACHE and have_recent_cache(command, platform, language):
         data = load_page_from_cache(command, platform, language)
     else:
+        if only_use_cache:
+            raise CacheNotExist("Cache for {} in {} not Found".format(
+                    command, platform
+                ))
         page_url = get_page_url(command, platform, remote, language)
         try:
             data = urlopen(
@@ -196,6 +205,16 @@ def get_page(command, remote=None, platforms=None, languages=None):
         platforms = get_platform_list()
     if languages is None:
         languages = get_language_list()
+    # only use cache
+    for platform in platforms:
+        for language in languages:
+            if platform is None:
+                continue
+            try:
+                return get_page_for_platform(
+                    command, platform, remote, language, only_use_cache=True)
+            except CacheNotExist:
+                continue
     for platform in platforms:
         for language in languages:
             if platform is None:
