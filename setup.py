589df86214938fb440d7bf88112fa1d2873e7631
Casper da Costa-Luis <casper.dcl@physics.org>
2021-05-03 14:43:18 +0100

    bump shtab version to fix zsh completion

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
             "tldr = tldr:cli"
         ]
     },
-    install_requires=['termcolor', 'colorama', 'shtab>=1.3.0'],
+    install_requires=['termcolor', 'colorama', 'shtab>=1.3.6'],
     tests_require=[
         'pytest',
         'pytest-runner',
