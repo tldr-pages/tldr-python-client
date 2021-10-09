fb114ee5347e011936b26640552067d90af4560d
Matthew Peveler <matt.peveler@gmail.com>
2021-10-09 08:39:48 -1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 project = 'tldr'
 copyright = '2014, Felix Yan'
 author = 'Felix Yan'
-from tldr import __version__
+from tldr import __version__  # noqa: E402
 release = __version__
 
 # -- General configuration ---------------------------------------------------
