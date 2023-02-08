7bcb7cdc7eeea85db3a8b8d70bd116068a7cc091
Jaime <jaipas01@ucm.es>
2023-02-08 10:36:38 +0100

    Update the code style

--- tldr.py
+++ tldr.py
@@ -622,7 +622,7 @@ def main() -> None:
                 output(results[0][0], plain=options.markdown)
                 if results[1:]:
                     platform_str = results[1][1]
-                    print(f"There are other versions of this page under the {platform_str} platform.")
+                    print("There are other versions of this page under the {} platform.".format(platform_str))
         except URLError as e:
             sys.exit("Error fetching from tldr: {}".format(e))
 
