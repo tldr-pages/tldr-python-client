4595993edc26c0a51d926c53d86571e6947d6492
Patrice Denis <patricedenis@users.noreply.github.com>
2024-06-26 15:38:21 +0000

    Update tldr.py : fix syntax error in def of OS_DIRECTORIES

--- tldr.py
+++ tldr.py
@@ -41,7 +41,7 @@ if int(os.environ.get('TLDR_ALLOW_INSECURE', '0')) == 1:
     URLOPEN_CONTEXT.check_hostname = False
     URLOPEN_CONTEXT.verify_mode = ssl.CERT_NONE
 
-OS_DIRECTORIES = {
+ = {
     "android": "android",
     "darwin": "osx",
     "freebsd": "freebsd",
@@ -51,7 +51,7 @@ OS_DIRECTORIES = {
     "openbsd": "openbsd",
     "osx": "osx",
     "sunos": "sunos",
-    "win32": "windows"
+    "win32": "windows",
     "windows": "windows"
 }
 
