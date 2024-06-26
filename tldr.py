48c38579b02b9345b1d0e2b0fb049f0a3f7e53ac
Patrice Denis <patricedenis@users.noreply.github.com>
2024-06-26 16:55:38 +0000

    Update tldr.py :  fix too long parser platform options
    add multi line support

--- tldr.py
+++ tldr.py
@@ -519,9 +519,11 @@ def create_parser() -> ArgumentParser:
         nargs=1,
         default=None,
         type=str,
-        choices=['android', 'freebsd', 'linux', 'netbsd', 'openbsd', 'osx', 'sunos', 'windows', 'common'],
+        choices=['android', 'freebsd', 'linux', 'netbsd', 'openbsd', 'osx', 'sunos',
+                 'windows', 'common'],
         metavar='PLATFORM',
-        help="Override the operating system [android, freebsd, linux, netbsd, openbsd, osx, sunos, windows, common]"
+        help="Override the operating system [android, freebsd, linux, netbsd, openbsd,"
+                 "osx, sunos, windows, common]"
     )
 
     parser.add_argument('-l', '--list',
